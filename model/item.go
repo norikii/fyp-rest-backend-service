f2b2d7fffa8517cd7b32a948a41acd8863e96b2a
tatrasoft <n.nazarej@gmail.com>
2020-04-24 09:00:23 +0100

    models have been updated

--- model/item.go
+++ model/item.go
@@ -9,7 +9,7 @@ type Item struct {
 	ItemType string `json:"item_type,omitempty" bson:"item_type,omitempty"`
 	ItemImg string `json:"item_img,omitempty" bson:"item_img,omitempty"`
 	ItemPrice float32 `json:"item_price,omitempty" bson:"item_price,omitempty"`
-	EstimatePrepareTime  int64 `json:"estimate_prepare_time,omitempty" bson:"estimate_prepare_time,omitempty"`
+	EstimatePrepareTime  int `json:"estimate_prepare_time,omitempty" bson:"estimate_prepare_time,omitempty"`
 	CreatedAt int64 `json:"created_at,omitempty" bson:"created_at,omitempty"`
 	UpdatedAt int64 `json:"updated_at,omitempty" bson:"updated_at,omitempty"`
 	DeletedAt int64 `json:"deleted_at,omitempty" bson:"deleted_at,omitempty"`
--- model/order.go
+++ model/order.go
@@ -5,8 +5,8 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 type Order struct {
 	ID primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 	TableID int32 `json:"table_id,omitempty" bson:"table_id,omitempty"`
-	StaffUserID primitive.ObjectID `json:"staff_user_id,omitempty" bson:"staff_user_id,omitempty"`
-	GuestUserID primitive.ObjectID `json:"guest_user_id,omitempty" bson:"guest_user_id,omitempty"`
+	StaffUserID string `json:"staff_user_id,omitempty" bson:"staff_user_id,omitempty"`
+	GuestUserID string `json:"guest_user_id,omitempty" bson:"guest_user_id,omitempty"`
 	Items []Item `json:"items,omitempty" bson:"items,omitempty"`
 	TotalPrice float32 `json:"total_price,omitempty" bson:"total_price,omitempty"`
 	CreatedAt int64 `json:"created_at,omitempty" bson:"created_at,omitempty"`
